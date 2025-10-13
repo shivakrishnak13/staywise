482b2fbeb22214a537e4e3ed377c72a070cb5b7f
Shivakrishna <kosarishivakrishna10@gmail.com>
2025-10-13 13:28:02 +0530

    configured cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,14 @@ import { authenticate } from "./middlewares/authMiddleware";
 import bookingsRouter from './routes/bookings.routes';
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: [
+        'https://staywise-delta.vercel.app',
+        'http://localhost:3000'
+    ],
+    credentials: true
+}));
+
 app.use(express.json())
 
 app.use('/api/user', authRouter)
